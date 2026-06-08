2b1921cf1df9b1f2004a5596cb82a118ae6ee6ae
nvs13sasmita <nvs13sasmita@gmail.com>
2026-06-08 18:09:45 +0700

    feat: add test_cam.py script to verify camera capture functionality

---
+++ test_cam.py
@@ -0,0 +1,17 @@
+import cv2
+cap = cv2.VideoCapture(0, cv2.CAP_v4L2)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+print("opened =", cap.isOpened())
+
+ret, frame = cap.read()
+print("ret =", ret)
+
+if ret:
+    print(frame.shape)
+    cv2.imwrite("test.jpg", frame)
+    print("saved test.jpg")
+
+cap.release()
